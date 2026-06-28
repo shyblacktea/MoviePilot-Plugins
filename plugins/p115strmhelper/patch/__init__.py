34e937720854afd157582ca8a18cff641dd8f6b3
shyblacktea <33568732+shyblacktea@users.noreply.github.com>
2026-06-28 19:09:41 +0800

    Publish P115StrmHelper v2.8.49

---
+++ plugins/p115strmhelper/patch/__init__.py
@@ -0,0 +1,6 @@
+from .u115_open import U115Patcher
+from .transfer_chain import TransferChainPatcher
+from .p115disk_upload import P115DiskPatcher
+
+
+__all__ = ["U115Patcher", "TransferChainPatcher", "P115DiskPatcher"]
