f5392837bcd4306066eb77cb9c5ac5688610c677
shyblacktea <33568732+shyblacktea@users.noreply.github.com>
2026-07-06 18:42:49 +0800

    SubscribePlus v0.15: 记录删除/搜索其他站点菜单/站点名称显示

--- plugins.v2/subscribeplus/dist/assets/remoteEntry.js
+++ plugins.v2/subscribeplus/dist/assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./Page":()=>{
-      dynamicLoadingCss(["__federation_expose_Page-D1lQEIv2.css"], false, './Page');
-      return __federation_import('./__federation_expose_Page-B65UaLi9.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
+      dynamicLoadingCss(["__federation_expose_Page-AenubXW0.css"], false, './Page');
+      return __federation_import('./__federation_expose_Page-x1ZpJnJu.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
 "./Config":()=>{
       dynamicLoadingCss(["__federation_expose_Config-D-wbZoBA.css"], false, './Config');
       return __federation_import('./__federation_expose_Config-4uwmuKII.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
