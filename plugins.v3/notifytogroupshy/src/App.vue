c5067cb8e4f87128237c19b22521038ad36cbb70
shyblacktea <33568732+shyblacktea@users.noreply.github.com>
2026-09-12 13:14:00 +0800

    Publish NotifyToGroupShy v0.0.1

---
+++ plugins.v3/notifytogroupshy/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Page :api="props.api" @close="emit('close')" />
+</template>
+
+<script setup>
+import Page from './components/Page.vue'
+
+const props = defineProps({
+  api: { type: Object, default: () => ({}) },
+})
+const emit = defineEmits(['close'])
+</script>
